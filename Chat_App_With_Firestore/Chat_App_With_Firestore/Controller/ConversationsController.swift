2595089b2aa1b8990edc96eb2f5fd4f96201fc46
YounByoungil <jimmy7525@naver.com>
2021-02-17 01:37:39 +0900

    Setup Home Screen Navigation Bar

--- Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/ConversationsController.swift
+++ Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/ConversationsController.swift
@@ -14,6 +14,7 @@ class ConversationsController : UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+    configureNavigationBar()
   }
   
   //MARK: - Selectors
@@ -25,10 +26,27 @@ class ConversationsController : UIViewController {
   func configureUI() {
     view.backgroundColor = .systemBackground
     
+    let image = UIImage(systemName: "person.circle.fill")
+    navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+  }
+  
+  func configureNavigationBar() {
+    let appearance = UINavigationBarAppearance() // 네비게이션 바 백그라운드 appearance 생성
+    appearance.configureWithOpaqueBackground()
+    appearance.largeTitleTextAttributes = [.foregroundColor : UIColor.white] // 'Message' 타이틀 흰색으로 나타내기
+    appearance.backgroundColor = .systemPurple
+    
+    navigationController?.navigationBar.standardAppearance = appearance
+    navigationController?.navigationBar.compactAppearance = appearance
+    navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    
     navigationController?.navigationBar.prefersLargeTitles = true
     navigationItem.title = "Messages"
     
-    let image = UIImage(systemName: "person.circle.fill")
-    navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    navigationController?.navigationBar.tintColor = .white
+    navigationController?.navigationBar.isTranslucent = true  // 반투명한
+    
+    navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
+    
   }
 }
