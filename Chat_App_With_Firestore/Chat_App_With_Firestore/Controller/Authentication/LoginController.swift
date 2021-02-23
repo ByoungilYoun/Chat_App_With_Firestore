6fd64a07b282a87e9f9b44bc49b504427b09256d
YounByoungil <jimmy7525@naver.com>
2021-02-24 00:31:31 +0900

    Finish Login Screen UI

--- Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/Authentication/LoginController.swift
+++ Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/Authentication/LoginController.swift
@@ -31,8 +31,9 @@ class LoginController : UIViewController {
     let button = UIButton(type: .system)
     button.setTitle("Log In", for: .normal)
     button.layer.cornerRadius = 5
-    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-    button.backgroundColor = .systemRed
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+    button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    button.setTitleColor(.white, for: .normal)
     button.setHeight(height: 50)
     return button
   }()
@@ -44,10 +45,28 @@ class LoginController : UIViewController {
     tf.isSecureTextEntry = true
     return tf
   }()
+  
+  private let dontHaveAccountButton : UIButton = {
+    let button = UIButton(type: .system)
+    let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ",
+                                                    attributes: [.font : UIFont.systemFont(ofSize: 16),
+                                                                 .foregroundColor : UIColor.white])
+    attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [.font : UIFont.boldSystemFont(ofSize: 16),
+                                                                              .foregroundColor : UIColor.white]))
+    button.setAttributedTitle(attributedTitle, for: .normal)
+    button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+    return button
+  }()
   //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+  }
+  
+  //MARK: - Selectors
+  @objc func handleShowSignUp() {
+   let controller = RegistrationController()
+    navigationController?.pushViewController(controller, animated: true)
   }
   
   //MARK: - Helpers
@@ -72,6 +91,10 @@ class LoginController : UIViewController {
     view.addSubview(stack)
     stack.anchor(top : iconImage.bottomAnchor, left : view.leftAnchor, right : view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: -32)
     
+    view.addSubview(dontHaveAccountButton)
+    dontHaveAccountButton.anchor(left : view.leftAnchor,
+                                 right : view.rightAnchor,
+                                 bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingLeft: 32, paddingRight: -32)
   }
   
   func configureGradientLayer() {
