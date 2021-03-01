8e0f62c8ea891d4adbf813c7dae184564fbcfc58
YounByoungil <jimmy7525@naver.com>
2021-03-01 20:47:27 +0900

    Custom Input Accessory View For Sending Messages

--- Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/ChatController.swift
+++ Chat_App_With_Firestore/Chat_App_With_Firestore/Controller/ChatController.swift
@@ -11,6 +11,12 @@ class ChatController : UICollectionViewController {
   
   //MARK: - Properties
   private let user : User
+  
+  // 메세지 텍스트뷰와 send 버튼 있는 커스텀 뷰
+  private lazy var customInputView : CustomInputAccessoryView = {
+    let iv = CustomInputAccessoryView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+    return iv
+  }()
   //MARK: - Lifecycle
   
   init(user : User) {
@@ -28,8 +34,21 @@ class ChatController : UICollectionViewController {
     print("Debug : User in chat controller is \(user.username)")
   }
   
+  // customInputView 를 할때 해준다.
+  override var inputAccessoryView: UIView? {
+    get {
+      return customInputView
+    }
+  }
+  
+  // customInputView 를 할때 해준다.
+  override var canBecomeFirstResponder: Bool {
+    return true
+  }
+  
   //MARK: - Helpers
   func configureUI() {
     collectionView.backgroundColor = .white
+    configureNavigationBar(withTitle: user.username, prefersLargeTitle: false)
   }
 }
---
+++ Chat_App_With_Firestore/Chat_App_With_Firestore/View/CustomInputAccessoryView.swift
@@ -0,0 +1,78 @@
+//
+//  CustomInputAccessoryView.swift
+//  Chat_App_With_Firestore
+//
+//  Created by 윤병일 on 2021/03/01.
+//
+
+import UIKit
+
+class CustomInputAccessoryView : UIView {
+  //MARK: - Properties
+  
+  private let messageInputTextView : UITextView = {
+    let tv = UITextView()
+    tv.font = UIFont.systemFont(ofSize: 16)
+    tv.isScrollEnabled = false
+    return tv
+  }()
+  
+  private lazy var sendButton : UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Send", for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+    button.setTitleColor(.systemPurple, for: .normal)
+    button.addTarget(self, action: #selector(handleSendMessage), for: .touchUpInside)
+    return button
+  }()
+  
+  private let placeholderLabel : UILabel = {
+    let label = UILabel()
+    label.text = "Enter Message"
+    label.font = UIFont.systemFont(ofSize: 16)
+    label.textColor = .lightGray
+    return label
+  }()
+  //MARK: - Lifecycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .white
+    autoresizingMask = .flexibleHeight
+    
+    layer.shadowOpacity = 0.25
+    layer.shadowRadius = 10
+    layer.shadowOffset = .init(width: 0, height: -8)
+    layer.shadowColor = UIColor.lightGray.cgColor
+    
+    addSubview(sendButton)
+    sendButton.anchor(top : topAnchor, right: rightAnchor, paddingTop: 4, paddingRight: -8)
+    sendButton.setDimensions(height: 50, width: 50)
+    
+    addSubview(messageInputTextView)
+    messageInputTextView.anchor(top : topAnchor, left: leftAnchor, right: sendButton.leftAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, paddingTop: 12, paddingLeft: 4, paddingRight: -8, paddingBottom: -8)
+    
+    addSubview(placeholderLabel)
+    placeholderLabel.anchor(left : messageInputTextView.leftAnchor, paddingLeft: 4)
+    placeholderLabel.centerY(inView: messageInputTextView)
+    
+    // UITextView 에 placeholder 속성이 없어서 라벨로 대체했는데 글씨를 쓸때 안보이게 하기 위해서 노티피케이션센터 이용
+    NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    return .zero
+  }
+  
+  //MARK: - Selectors
+  @objc func handleSendMessage() {
+    print("Debug : Handle send message here..")
+  }
+  
+  @objc func handleTextInputChange() {
+    placeholderLabel.isHidden = !self.messageInputTextView.text.isEmpty
+  }
+}
